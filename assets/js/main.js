5227ab148c23f362c5ded3bf343ed3113c4ae35a
Yolosopher <nika.nishnianidze.10@gmail.com>
2021-03-09 03:24:23 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// set LANG ACTIVE
+const langActiveLiAINTEXT = document.querySelector('.languagebar__ul li.active a').innerText
+const activeLangSpan = document.querySelector('.languagebar__active')
+
+activeLangSpan.innerText = langActiveLiAINTEXT
+
