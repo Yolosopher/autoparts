c9c7ff88b8801bed4e219db8a1fb189f8fb1ee95
Yolosopher <nika.nishnianidze.10@gmail.com>
2021-03-11 14:52:59 +0400

    homepage expect searchbar and mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,90 @@
 // set LANG ACTIVE
-const langActiveLiAINTEXT = document.querySelector('.languagebar__ul li.active a').innerText
+const langActiveLiAINTEXT = document.querySelector(
+	'.languagebar__ul li.active a'
+).innerText
 const activeLangSpan = document.querySelector('.languagebar__active')
-
 activeLangSpan.innerText = langActiveLiAINTEXT
 
+const header = document.querySelector('header')
+let headerHeight = header.offsetHeight
+
+const headerBotFixer = () => {
+	let opacityValue =
+		window.pageYOffset >= 2 * headerHeight
+			? 1
+			: (window.pageYOffset - headerHeight) / 100
+
+	if (window.pageYOffset > headerHeight) {
+		header.style.backgroundColor = `rgba(32, 72, 157, ${opacityValue})`
+	} else {
+		header.style.backgroundColor = `rgba(32, 72, 157, 0)`
+	}
+}
+headerBotFixer()
+window.addEventListener(
+	'scroll',
+	() => {
+		headerBotFixer()
+	},
+	false
+)
+let hmpgTopProducts = new Swiper('.topProducts__slider', {
+	spaceBetween: 24,
+	slidesPerView: 'auto',
+	// observeParents: true,
+	// observer: true,
+	pagination: {
+		el: '.topProducts__slider__pagination',
+		type: 'bullets',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.topProducts__slider__navRight',
+		prevEl: '.topProducts__slider__navLeft',
+	},
+})
+
+const addInBasketMessage = document.querySelector('.productAddMessage')
+const addBtns = document.querySelectorAll(
+	'.topProducts__slider__wrapper__slide__addbtn'
+)
+const inCartNum = document.querySelector(
+	'.header__bottom__shoppingcart__number'
+)
+
+addBtns.forEach((btn) => {
+	btn.addEventListener('click', async (e) => {
+		e.preventDefault()
+		let productId = btn.getAttribute('data-id')
+
+		let response
+		try {
+			// response = await axios.post(addProductURL, {
+			// 	id: productId
+			// })
+			response = {
+				data: {
+					productsInBasket: 5,
+				},
+			}
+		} catch (error) {
+			console.log(error.message)
+		}
+
+		if (!response.data) {
+			addInBasketMessage.classList.add('error')
+			addInBasketMessage.classList.add('shown')
+			setTimeout(() => {
+				addInBasketMessage.classList.remove('shown')
+			}, 3000)
+		} else {
+			inCartNum.innerText = response.data.productsInBasket
+			addInBasketMessage.classList.remove('error')
+			addInBasketMessage.setAttribute('href', `${window.location.origin}/products/${productId}`)
+			addInBasketMessage.classList.add('shown')
+			setTimeout(() => {
+				addInBasketMessage.classList.remove('shown')
+			}, 3000)
+		}
+	})
+})
